7fafbb9777a06d4b2c8e35a06bbf4492121e8a01
Pablo <pablo@maldonado.cz>
2021-11-10 11:19:45 +0100

    working auth

--- api/views.py
+++ api/views.py
@@ -1,15 +1,17 @@
-from rest_framework.decorators import permission_classes
 from api.models import Product
 from api.serializers import ProductSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import permissions
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 class ProductListView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
 
 class ProductDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
