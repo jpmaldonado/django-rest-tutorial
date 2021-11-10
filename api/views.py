add520eb892ffb80a7c4cc06d2fed368f24472e5
Pablo <pablo@maldonado.cz>
2021-11-10 09:39:20 +0100

    DRF-style

--- api/views.py
+++ api/views.py
@@ -2,25 +2,23 @@ from django.shortcuts import render
 from rest_framework import serializers
 from api.models import Product
 from api.serializers import ProductSerializer
-from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
-@csrf_exempt
+@api_view(['GET','POST'])
 def product_list(request):
     if request.method == 'GET':
         products = Product.objects.all()
         serializer = ProductSerializer(instance=products, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ProductSerializer(data=data)
+        serializer = ProductSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=201)
     
-        return JsonResponse(serializer.errors, status=400)
+        return Response(serializer.errors, status=400)
